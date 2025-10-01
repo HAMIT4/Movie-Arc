c88c295c4633d8a64f47e3479f2496c92ee347c8
HAMIT4 <hamit.ht4@gmail.com>
2025-10-01 16:43:02 +0300

    Search ui complete

---
+++ app/src/main/java/com/hamit/moviearc/Adapters/SearchRecycler.java
@@ -0,0 +1,81 @@
+package com.hamit.moviearc.Adapters;
+
+import static com.hamit.moviearc.Network.Services.TmdbService.IMAGE_BASE_URL;
+import static com.hamit.moviearc.Network.Services.TmdbService.IMAGE_SIZE_W342;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.hamit.moviearc.Network.Data.MultiSearchResponse;
+import com.hamit.moviearc.R;
+
+import java.util.List;
+
+public class SearchRecycler extends RecyclerView.Adapter<SearchRecycler.MyViewHolder>{
+
+    private List<MultiSearchResponse.ResultItem> searchResponse;
+
+    public SearchRecycler(List<MultiSearchResponse.ResultItem> searchResponse) {
+        this.searchResponse = searchResponse;
+    }
+
+    public void updateResults(List<MultiSearchResponse.ResultItem> searchedMovies) {
+        this.searchResponse = searchedMovies;
+        notifyDataSetChanged();
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        MyViewHolder holder= new MyViewHolder(LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.potrait_movie_item2,parent,false));
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+
+        MultiSearchResponse.ResultItem movie= searchResponse.get(position);
+
+        holder.movieTitle.setText(movie.getTitle());
+        double rating = movie.getVoteAverage();
+        holder.movieRating.setText(String.format("%.1f", rating));
+        String releaseYear = movie.getReleaseDate() != null && movie.getReleaseDate().length() >= 4 ?
+                movie.getReleaseDate().substring(0, 4) : "N/A";
+        holder.movieRelease.setText(releaseYear);
+
+        String fullPosterUrl= IMAGE_BASE_URL + IMAGE_SIZE_W342 + movie.getPosterPath();
+        Glide.with(holder.itemView.getContext())
+                .load(fullPosterUrl)
+                .placeholder(R.drawable.placeholder)
+                .error(R.drawable.error_image)
+                .into(holder.movieImage);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return searchResponse == null ? 0 : searchResponse.size();
+    }
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        private ImageView movieImage;
+        private TextView movieTitle, movieRating, movieRelease;
+        public MyViewHolder(@NonNull View itemView) {
+            super(itemView);
+            movieImage = itemView.findViewById(R.id.posterImage);
+            movieTitle = itemView.findViewById(R.id.movieTitle);
+            movieRating = itemView.findViewById(R.id.movieRating);
+            movieRelease = itemView.findViewById(R.id.movieRelease);
+        }
+    }
+}
+
+
--- app/src/main/java/com/hamit/moviearc/Fragments/SearchFragment/SearchFragment.java
+++ app/src/main/java/com/hamit/moviearc/Fragments/SearchFragment/SearchFragment.java
@@ -1,5 +1,7 @@
 package com.hamit.moviearc.Fragments.SearchFragment;
 
+import static com.hamit.moviearc.Fragments.HomeFragment.HomeFragment.API_KEY;
+
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
@@ -7,6 +9,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
@@ -14,21 +17,36 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.material.chip.Chip;
 import com.google.android.material.chip.ChipGroup;
+import com.hamit.moviearc.Adapters.SearchRecycler;
+import com.hamit.moviearc.Network.Clients.RetrofitClient;
+import com.hamit.moviearc.Network.Data.MultiSearchResponse;
+import com.hamit.moviearc.Network.Services.TmdbService;
 import com.hamit.moviearc.R;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class SearchFragment extends Fragment {
 
-
+    private SearchView searchView;
     private SearchViewModel mViewModel;
     private FrameLayout noSearch;
     private RecyclerView searchResultRecycler;
+    private TmdbService mdbService;
+    private SearchRecycler searchAdapter;
+    ChipGroup chipGroup;
+    private TextView popularTxt;
 
     public static SearchFragment newInstance() {
         return new SearchFragment();
@@ -39,12 +57,20 @@ public class SearchFragment extends Fragment {
                              @Nullable Bundle savedInstanceState) {
 
         View contentView= inflater.inflate(R.layout.fragment_search, container, false);
+        mdbService= RetrofitClient.getTmdbService();
 
         // initializing components
-        ChipGroup chipGroup= contentView.findViewById(R.id.popularSearchesGroup);
-        SearchView searchView= contentView.findViewById(R.id.Search_searchView);
+        chipGroup= contentView.findViewById(R.id.popularSearchesGroup);
+        searchView= contentView.findViewById(R.id.Search_searchView);
         noSearch= contentView.findViewById(R.id.frameSearch);
+        popularTxt= contentView.findViewById(R.id.txt);
+
         searchResultRecycler= contentView.findViewById(R.id.searchResultRecycler);
+        searchResultRecycler.setLayoutManager(new GridLayoutManager(getContext(), 2));
+
+        searchAdapter= new SearchRecycler(Collections.emptyList());
+        searchResultRecycler.setAdapter(searchAdapter);
+
 
         // popular searches default
         List<String> popularSearches= Arrays.asList(
@@ -74,6 +100,8 @@ public class SearchFragment extends Fragment {
                 if (newText.trim().isEmpty()){
                     noSearch.setVisibility(View.VISIBLE);
                     searchResultRecycler.setVisibility(View.GONE);
+                    chipGroup.setVisibility(View.VISIBLE);
+                    popularTxt.setVisibility(View.VISIBLE);
                 }
                 return true;
             }
@@ -82,10 +110,14 @@ public class SearchFragment extends Fragment {
             public boolean onQueryTextSubmit(String query) {
                 if (!query.trim().isEmpty()){
                     noSearch.setVisibility(View.GONE);
+                    chipGroup.setVisibility(View.GONE);
+                    popularTxt.setVisibility(View.GONE);
+
                     // set recycler view to visible
                     searchResultRecycler.setVisibility(View.VISIBLE);
 
                     // TODO: call Api and viewModel to fetch search results
+                    fetchSearchResults(query);
 
                 }
                 return true;
@@ -93,6 +125,29 @@ public class SearchFragment extends Fragment {
         });
 
         return contentView;
+    }
+
+    private void fetchSearchResults(String query) {
+        Call<MultiSearchResponse> call= mdbService.multiSearch(API_KEY, query, 1, false);
+        call.enqueue(new Callback<MultiSearchResponse>() {
+            @Override
+            public void onResponse(Call<MultiSearchResponse> call, Response<MultiSearchResponse> response) {
+                if (response.isSuccessful() && response.body() != null){
+                    MultiSearchResponse searchResponse= response.body();
+                    List<MultiSearchResponse.ResultItem> results= searchResponse.getResults();
+                    // add the results to a recyclerView to display them
+                    searchAdapter.updateResults(results);
+
+                } else {
+                    Toast.makeText(getContext(), "No Results found", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<MultiSearchResponse> call, Throwable t) {
+                Toast.makeText(getContext(), "Error fetching search results", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
     @Override
